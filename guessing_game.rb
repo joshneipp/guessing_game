d21599eca68b788ec0dfc1ecb0fc611a70cdac93
joshneipp <joshneipp@gmail.com>
2015-09-01 20:58:34 -0400

    Catch duplicate guesses.

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,18 +3,29 @@
 
 #Computer generates a secret number.
 secret_num = rand(1..99)
+guess_array = []
 
 def user_input
   puts "Number please: "
   gets.chomp.to_i
 end
 
+def test_duplicates(array_x, var_y)
+  if array_x.include?(var_y)
+    puts "Doh! No duplicates!"
+  else
+    return false
+  end
+end
 
-def guess_check(secret_num)
+def guess_check(secret_num, guess_array)
   count = 0
   while count < 5 do
     count += 1
     human_guess = user_input
+    if !test_duplicates(guess_array, human_guess)
+      guess_array << human_guess
+    end
     if human_guess < secret_num
       puts "That's too low."
     elsif human_guess > secret_num
@@ -26,4 +37,4 @@ def guess_check(secret_num)
   end
 end
 
-guess_check(secret_num)
+guess_check(secret_num, guess_array)
