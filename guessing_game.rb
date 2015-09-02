c0f0ed314c87b4c2716997aa37940acbd68f9176
joshneipp <joshneipp@gmail.com>
2015-09-01 21:39:54 -0400

    Define comparison method.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,8 +1,10 @@
 #In this game, a user has 5 chances to guess a number between 1 and 100
 #chosen at random by the computer.
 
-#Computer generates a secret number.
-secret_num = rand(1..99)
+secret_array = (1..99).to_a
+#secret_array.shuffle!
+secret_num = secret_array.shuffle.sample
+#secret_num = rand(1..99)
 guess_array = []
 
 def user_input
@@ -18,6 +20,17 @@ def test_duplicates(array_x, var_y)
   end
 end
 
+def compare(var1, var2)
+  if var1 < var2
+    return "That's too low."
+  elsif var1 > var2
+    return "That's too high."
+  elsif var1 == var2
+    return "You got it!"
+  end
+
+end
+
 def guess_check(secret_num, guess_array)
   count = 0
   while count < 5 do
@@ -26,19 +39,12 @@ def guess_check(secret_num, guess_array)
     if !test_duplicates(guess_array, human_guess)
       guess_array << human_guess
     end
-    if human_guess < secret_num
-      puts "That's too low."
-    elsif human_guess > secret_num
-      puts "That's too high."
-    elsif human_guess == secret_num
-      puts "You got it!"
-      break
-    end
-    if count == 5
+    puts compare(human_guess, secret_num)
+    if count == 5 && human_guess != secret_num
       puts "Too bad! You fail!"
     end
   end
 end
 
 guess_check(secret_num, guess_array)
-puts "The secret number was #{secret_num}"
+puts "The secret number was #{secret_num}."
